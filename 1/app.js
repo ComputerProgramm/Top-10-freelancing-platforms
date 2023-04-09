09978ab903d7a90aae33b541c50f912d78f6b963
ComputerProgramm <109730216+ComputerProgramm@users.noreply.github.com>
2023-04-09 17:44:08 +0500

    Add files via upload

---
+++ 1/app.js
@@ -0,0 +1,29 @@
+function Up(){
+    window.location.href = 'https://www.upwork.com/'
+}
+function Guru(){
+    window.location.href = 'https://www.guru.com/'
+}
+function  Top(){
+    window.location.href = 'https://www.toptal.com/'
+}
+function  Free(){
+    window.location.href = 'https://www.freelancer.com/'
+}
+function  des(){
+    window.location.href = 'https://99designs.com    '
+}
+function  Per(){
+    window.location.href = 'https://www.peopleperhour.com/'
+}
+function  list(){
+    window.location.href = 'https://www.craigslist.org/about/sites'
+}function In(){
+    window.location.href =' https://www.linkedin.com'
+}
+function Fiv(){
+    window.location.href =' https://www.fiverr.com'
+}
+function Be(){
+    window.location.href =' https://www.behance.net/'
+}
